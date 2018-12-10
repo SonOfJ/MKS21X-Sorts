891ccde984644c6045bb7cb37f639f5a1ea552bf
Jason Zheng <zzheng3@stuy.edu>
2018-12-10 11:15:29 -0500

    considered cases with empty arrays

--- Sorts.java
+++ Sorts.java
@@ -1,30 +1,34 @@
 import java.util.Random;
 public class Sorts{
   public static void selectionSort(int[] ary) {
-    for(int i = 0; i < ary.length - 1; i = i + 1) { //General loop
-      int chosen = ary[i]; //Chosen will change depending on the size of the other elements
-      int index = i; //This is going to be the index of the chosen element
-      for(int j = i + 1; j < ary.length; j = j + 1) { //This is the loop that will actually be finding the smallest element
-        if (ary[j] < chosen) { //If the element is smaller than chosen
-          chosen = ary[j]; //Make that element the chosen
-          index = j; //Record the index of the chosen element
+    if (ary.length != 0) {
+      for(int i = 0; i < ary.length - 1; i = i + 1) { //General loop
+        int chosen = ary[i]; //Chosen will change depending on the size of the other elements
+        int index = i; //This is going to be the index of the chosen element
+        for(int j = i + 1; j < ary.length; j = j + 1) { //This is the loop that will actually be finding the smallest element
+          if (ary[j] < chosen) { //If the element is smaller than chosen
+            chosen = ary[j]; //Make that element the chosen
+            index = j; //Record the index of the chosen element
+          }
         }
+        ary[index] = ary[i]; //Substitue the chosen element with the old element
+        ary[i] = chosen; //Put the smallest element at the current index
       }
-      ary[index] = ary[i]; //Substitue the chosen element with the old element
-      ary[i] = chosen; //Put the smallest element at the current index
     }
   }
   public static void bubbleSort(int[] data) {
-    int ginga = data.length; //This will change every time the sort is completed
-    for(int i = 0; i < data.length - 1; i = i + 1) { //First loop that runs exactly one less times than the length
-      for(int j = 0; j < ginga - 1; j = j + 1) { //The second loop is the one that checks and swaps
-        if (data[j] > data[j + 1]) { //If the element is bigger than the one after it
-          int element = data[j]; //Store element
-          data[j] = data[j + 1]; //Element becomes the one after it
-          data[j + 1] = element; //The one after it becomes the element
+    if (data.length != 0) {
+      int ginga = data.length; //This will change every time the sort is completed
+      for(int i = 0; i < data.length - 1; i = i + 1) { //First loop that runs exactly one less times than the length
+        for(int j = 0; j < ginga - 1; j = j + 1) { //The second loop is the one that checks and swaps
+          if (data[j] > data[j + 1]) { //If the element is bigger than the one after it
+            int element = data[j]; //Store element
+            data[j] = data[j + 1]; //Element becomes the one after it
+            data[j + 1] = element; //The one after it becomes the element
+          }
         }
+        ginga = ginga - 1; //We know for the fact that the largest element is at the end now so we don't check the last one
       }
-      ginga = ginga - 1; //We know for the fact that the largest element is at the end now so we don't check the last one
     }
   }
   public static String print(int[] data) { //Extra function for visual check
