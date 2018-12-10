67d5f080dc43a99fdffa12d7b47a8cf7cc4a8008
Jason Zheng <zzheng3@stuy.edu>
2018-12-09 21:36:37 -0500

    replaced == with equals in the main

--- Sorts.java
+++ Sorts.java
@@ -48,10 +48,10 @@ public class Sorts{
     int[] ha = new int[Integer.parseInt(args[0])]; //Creates a new integer array with the same size as specified by the first argument
     fillArray(ha);
     System.out.println("Initial: " + print(ha)); //Initial stage of array
-    if (args[1] == "selection") { //If the second argument is "selection," use the selection sort
+    if (args[1].equals("selection")) { //If the second argument is "selection," use the selection sort
       selectionSort(ha); //Perform the selection sort on the integer array
     }
-    if (args[1] == "bubble") { //If the second argument is "bubble," use the bubble sort
+    if (args[1].equals("bubble")) { //If the second argument is "bubble," use the bubble sort
       bubbleSort(ha); //Perform the bubble sort on the integer array
     }
     System.out.println("Final: " + print(ha)); //Sorted stage of array
